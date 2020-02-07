2323a201370bd71c313b842f6bb954352e1aaed9
Sean Moore <sean.moore@ni.com>
2020-02-07 16:36:40 -0600

    add rfsg playback extension project

---
+++ niRFSGPlayback/NIRfsgPlaybackExtensions/NIRfsgPlaybackExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Extension methods for simplifying datatype conversion between .NET and MATLAB
+
+using System;
+
+namespace NationalInstruments.ModularInstruments.NIRfsgPlayback
+{
+    public static class NIRfsgPlaybackExtensions
+    {
+        public static int DownloadUserWaveform(IntPtr rfsgSession, string waveformName, double[] real, double[] imaginary, double sampleInterval, bool burstPresent)
+        {
+            var iq = ComplexDouble.ComposeArray(real, imaginary);
+            var iqWfm = ComplexWaveform<ComplexDouble>.FromArray1D(iq);
+            iqWfm.PrecisionTiming = PrecisionWaveformTiming.CreateWithRegularInterval(new PrecisionTimeSpan(sampleInterval));
+            return NIRfsgPlayback.DownloadUserWaveform(rfsgSession, waveformName, iqWfm, burstPresent);
+        }
+
+        public static int ReadWaveformFromFileComplex(string filePath, out double[] real, out double[] imag, out double sampleInterval)
+        {
+            ComplexWaveform<ComplexDouble> outputWaveform = null;
+            int result = NIRfsgPlayback.ReadWaveformFromFileComplex(filePath, ref outputWaveform);
+            real = outputWaveform.GetRealDataArray(false);
+            imag = outputWaveform.GetImaginaryDataArray(false);
+            sampleInterval = outputWaveform.PrecisionTiming.SampleInterval.TotalSeconds;
+            return result;
+        }
+    }
+}
